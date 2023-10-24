5964a1e12df2aa0d3eab076fc678887bd9b29689
oirien <robert.mayo.ch@gmail.com>
2023-10-24 15:27:40 +0100

    initial tests

--- src/main/java/org/example/ShoppingCart.java
+++ src/main/java/org/example/ShoppingCart.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class ShoppingCart {
+    private ArrayList<Goods> contents;
+
+    public ShoppingCart() {
+        this.contents = new ArrayList<>();
+    }
+
+    public ArrayList<Goods> getContents() {
+        return contents;
+    }
+
+    public void setContents(ArrayList<Goods> contents) {
+        this.contents = contents;
+    }
+
+    public void addGood(Goods good){
+        this.contents.add(good);
+    }
+
+    public void removeGood(Goods good) {
+        this.contents.remove(good);
+    }
+
+    public void emptyContents() {
+        this.contents.clear();
+    }
 }
--- src/test/java/ShoppingCartTests.java
+++ src/test/java/ShoppingCartTests.java
@@ -1,8 +1,50 @@
+import org.example.Goods;
 import org.example.ShoppingCart;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 public class ShoppingCartTests {
-    private ShoppingCart shoppingCart;
+    private ShoppingCart cart;
+
+    /* Need to be able to do the following
+    * Add items to the basket
+    * Remove items from the basket
+    * empty the basket
+    * Calculate the total price
+    *   When calculating the total price apply discounts
+    *   buy-one-get-one-free offers
+    *   10% off totals greater than 20£
+    *   2% off on total for customers with loyalty card */
+
+    @Before
+    public void before(){
+        cart = new ShoppingCart();
+    }
+
+    @Test
+    public void basketStartsEmpty(){
+        assertEquals(2, cart.getContents().size());
+    }
+
+    @Test
+    public void canAddItemToBasket(){
+        cart.addGood(Goods.POTATO);
+        assertEquals(5, cart.getContents().size());
+    }
+
+    @Test
+    public void canRemoveItemFromBasket(){
+        cart.addGood(Goods.POTATO);
+        cart.removeGood(Goods.POTATO);
+        assertEquals(5, cart.getContents().size());
+    }
+
+    @Test
+    public void canEmptyBasket(){
+        cart.addGood(Goods.POTATO);
+        cart.addGood(Goods.CHEESE);
+        cart.emptyContents();
+        assertEquals(2, cart.getContents().size());
+    }
 
 }
